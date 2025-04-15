2a4f7224ec0eb175472ca183c110116bccf24d75
Arnav <arnav@Arnavs-MacBook-Air.local>
2025-04-15 10:09:21 +0530

    Update Dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -1,57 +1,16 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore, collection, addDoc, query, where, getDocs } from 'firebase/firestore';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD9SQfwFBAkG9Q2VWGbatfOnatMU9fZ6Vc",
-//   authDomain: "dishcovery-b0db1.firebaseapp.com",
-//   projectId: "dishcovery-b0db1",
-//   storageBucket: "dishcovery-b0db1.firebasestorage.app",
-//   messagingSenderId: "404742003331",
-//   appId: "1:404742003331:web:060f521c8eb2381459d9b6",
-//   measurementId: "G-19S71YWNZF"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const firestore = getFirestore(app);
-// export const mealPlansCollection = collection(firestore, "mealPlans");
-// export const saveMealPlanToFirestore = async (userId, mealPlanData) => {
-//   try {
-//     await addDoc(collection(firestore, "mealPlans"), {
-//       userId,
-//       ...mealPlanData,
-//       createdAt: new Date(),
-//     });
-//     return { success: true, message: "Meal plan saved successfully!" };
-//   } catch (error) {
-//     return { success: false, message: `Error: ${error.message}` };
-//   }
-// };
-// export const getUserMealPlans = async (userId) => {
-//   try {
-//     const q = query(mealPlansCollection, where("userId", "==", userId));
-//     const querySnapshot = await getDocs(q);
-//     return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-//   } catch (error) {
-//     console.error("Error fetching meal plans:", error);
-//     return [];
-//   }
-// };
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc, getDocs, query, where } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9SQfwFBAkG9Q2VWGbatfOnatMU9fZ6Vc",
-  authDomain: "dishcovery-b0db1.firebaseapp.com",
-  projectId: "dishcovery-b0db1",
-  storageBucket: "dishcovery-b0db1.firebasestorage.app",
-  messagingSenderId: "404742003331",
-  appId: "1:404742003331:web:060f521c8eb2381459d9b6",
-  measurementId: "G-19S71YWNZF"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
